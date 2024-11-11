7118fef99f62d5afd3848559678ec57ed59d4f22
Bittrance <bittrance@gmail.com>
2024-11-11 07:06:07 +0100

    Consumer can commit manually.

--- consume.py
+++ consume.py
@@ -3,9 +3,9 @@
 import asyncio
 import sys
 
-from aiokafka import AIOKafkaConsumer
+from aiokafka import AIOKafkaConsumer, TopicPartition
 
-async def consume(bootstrap_servers, group_id):
+async def consume(bootstrap_servers, group_id, auto):
     consumer = AIOKafkaConsumer(
         'bittrance-test',
         security_protocol='SASL_PLAINTEXT',
@@ -13,14 +13,20 @@ async def consume(bootstrap_servers, group_id):
         sasl_plain_username='user1',
         sasl_plain_password='w1ogDE4i8n',
         bootstrap_servers=bootstrap_servers,
-        group_id=group_id)
+        group_id=group_id,
+        enable_auto_commit=auto,
+    )
     await consumer.start()
+    await consumer.seek_to_end()
     try:
         async for msg in consumer:
             print("consumed: ", msg.topic, msg.partition, msg.offset,
                   msg.key, msg.value, msg.timestamp)
+            if not auto:
+                tp = TopicPartition(msg.topic, msg.partition)
+                await consumer.commit({tp: msg.offset + 1})
     finally:
         await consumer.stop()
 
 if __name__ == '__main__':
-    asyncio.run(consume(sys.argv[1], sys.argv[2]))
+    asyncio.run(consume(sys.argv[1], sys.argv[2], sys.argv[3] == 'auto'))
